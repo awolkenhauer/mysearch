99f00eaf6ab0693d7fd17a9c7b4d41c8af133b74
Alexander Wolkenhauer <wolkenhauera@gmail.com>
2020-04-03 21:59:55 +0000

    added string search

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ from indexsearch import index_search
 from timer import timer
 
 #Gather files to be searched
+
+directory = 'sample_text/'
+
 file_list = []
 
-for files in os.listdir("sample_text/"):
-  file_list.append(files)
 
+for file_name in os.listdir(directory):
+  file_path = os.path.join(directory, file_name)
+  file_list.append(file_path)
+
+
+print(file_list)
+
+search_term = ""
 
 #Take in user data
-search_term = input("Enter the search term: ") 
+while len(search_term) is 0:
+    search_term = input("Enter the search term: ").lower()
+    print("Please enter a valid search parameter")
 
 question = [
   inquirer.List('search_method',
@@ -22,21 +33,20 @@ question = [
                 carousel = True,
             ),
 ]
+
 answer = inquirer.prompt(question)
 
 start = timer()
 
 if answer["search_method"] == "String Match":
-    result = string_search()
+    result = string_search(search_term, file_list)
     print(result)
 elif answer["search_method"] == "Regular Expression":
-    result = reg_search()
+    result = reg_search(search_term, file_list) 
     print(result)
-    print(answer["search_method"])
 elif answer["search_method"] == "Indexed":
-    result = index_search()
+    result = index_search(search_term, file_list)
     print(result)
-    print(answer["search_method"])
 else:
     print(answer["search_method"] + "is not a valid selection")
 
--- stringsearch.py
+++ stringsearch.py
@@ -1,2 +1,7 @@
-def string_search():
-    return 1
+def string_search(search_term, file_list):
+    count = 0
+    for text in file_list:
+        with open(text.lower(), 'r') as fp:
+            for line in fp:
+                count += line.count(search_term)
+    return count
